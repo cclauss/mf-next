c9c6b0dabb7c7597d7ba29af7f21eab83d0211f3
cedricmoullet <cedric.moullet@gmail.com>
2013-02-14 08:49:44 +0100

    Add no cache for route document

--- geoadmin/views.py
+++ geoadmin/views.py
@@ -1,6 +1,6 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/index.pt')
+@view_config(route_name='home', renderer='templates/index.pt', http_cache=0)
 def my_view(request):
     return {'project': 'geoadmin'}
