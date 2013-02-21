7b8fdb4f5621bf0d9619c03da8afd35cea0bffa5
ltgal <ltgal@ip-10-51-55-74.eu-west-1.compute.internal>
2013-02-21 12:44:45 +0100

    very simple routing example

--- geoadmin/__init__.py
+++ geoadmin/__init__.py
@@ -1,5 +1,6 @@
 from pyramid.config import Configurator
 from pyramid.events import BeforeRender
+from geoadmin.controllers.helloworld import *
 from geoadmin import helpers
 
 def add_render_globals(event):
@@ -13,7 +14,11 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+
+    # Application specific
+    config.add_route('hello', '/hello/{name}')
+    config.add_view(hello_world, route_name='hello')
+
     config.add_subscriber(add_render_globals,BeforeRender)
     config.scan()
     return config.make_wsgi_app()
-
