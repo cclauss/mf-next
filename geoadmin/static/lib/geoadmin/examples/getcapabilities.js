a079103690ae2cb128bd0cb04641ec86cca4dcfc
loicgasser <loicgasser4@gmail.com>
2013-03-05 12:26:56 +0100

    assert error with google when parsing xml, update ol3

--- geoadmin/static/lib/geoadmin/examples/getcapabilities.js
+++ geoadmin/static/lib/geoadmin/examples/getcapabilities.js
@@ -4,20 +4,45 @@ goog.require('goog.debug.Console');
 goog.require('goog.debug.Logger');
 goog.require('goog.debug.Logger.Level');
 
-goog.require('goog.net.XhrIo');
+//goog.require('goog.net.XhrIo');
 
 goog.require('ol.parser.ogc.WMTSCapabilities_v1_0_0');
 
 window.onload = function() {
     'use strict';
 
+    if (goog.DEBUG) {
+        //install the console as output. There are other possible outputs (as seperate window, for instance)
+        var debugConsole = new goog.debug.Console();
+        debugConsole.setCapturing(true);
+        //configure ga namespace logging level
+        goog.debug.Logger.getLogger('ga').setLevel(goog.debug.Logger.Level.ALL);
+        //configure ol namespace logging level
+        goog.debug.Logger.getLogger('ol').setLevel(goog.debug.Logger.Level.OFF);
+    }
+
     var parser = new ol.parser.ogc.WMTSCapabilities_v1_0_0();
     var url = 'http://wmts.geo.admin.ch/1.0.0/WMTSCapabilities.xml';
 
-    goog.net.XhrIo.send(url, function(e) {
-        var xhr, obj, serviceIdentification, serviceProvider, operationsMetadata, contactInfo;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            var result = parser.read(xhr.responseXML);
+            console.log(result);
+        }
+    };
+    xhr.send();
+
+    /*goog.net.XhrIo.send(url, function(e) {
+        var xhr, xml, obj, serviceIdentification, serviceProvider, operationsMetadata, contactInfo;
+
         xhr = e.target;
-        obj = parser.read(xhr.getResponseXml());
+        xml = xhr.getResponseXml();
+        console.log(parser);
+
+        obj = parser.read(xml);
         serviceIdentification = obj.serviceIdentification;
         serviceProvider = obj.serviceProvider;
         operationsMetadata = obj.operationsMetadata;
@@ -25,5 +50,5 @@ window.onload = function() {
 
         console.log(serviceIdentification);
         console.log(serviceProvider);
-    });
+    });*/
 };
