1023b7071bc1ab9085674f9ea9594d9480762de6
loicgasser <loicgasser4@gmail.com>
2013-03-20 16:27:39 +0100

    simple sql alchemy connection example

---
+++ geoadmin/models/bod.py
@@ -0,0 +1,11 @@
+from geoadmin.models import Base
+from sqlalchemy import Column
+from sqlalchemy import Unicode
+from sqlalchemy import Integer
+from sqlalchemy import Text 
+
+class BodLayerFr(Base):
+    __tablename__ = 'view_bod_layer_info_fr'
+    #__table_args__ = ({'autoload': True})
+    id = Column('bod_layer_id', Text, primary_key=True)
+    bezeichnung = Column('bezeichnung', Text)
--- geoadmin/views/helloworld.py
+++ geoadmin/views/helloworld.py
@@ -1,4 +1,15 @@
 from pyramid.response import Response
+from pyramid.view import view_config
+
+from geoadmin.models import DBSession
+from geoadmin.models.bod import BodLayerFr
 
 def hello_world(request):
     return Response('Hello %(name)s!' % request.matchdict)
+
+#@view_config(renderer='json')
+def layer(request):
+    id = request.matchdict['id']
+    layer = DBSession.query(BodLayerFr).get(id)
+
+    return {'BodDescription': layer.bezeichnung}
