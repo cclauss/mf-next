492e54bc17412ecd7fe6da908fd4a8ad31219bb8
loicgasser <loicgasser4@gmail.com>
2013-03-27 13:50:06 +0100

    add map service class, change add view to decorator like style (more robust) and add models for the different languages

--- geoadmin/__init__.py
+++ geoadmin/__init__.py
@@ -6,11 +6,9 @@ from sqlalchemy import engine_from_config
 from geoadmin.models import initialize_sql
 from geoadmin.views.helloworld import *
 from geoadmin.views.layers import *
-from geoadmin.views.home import *
-
+from geoadmin.views.mapservice import *
 from geoadmin.lib import helpers
 
-engines = {}
 
 def add_render_globals(event):
     event['h'] = helpers
@@ -27,15 +25,13 @@ def main(global_config, **settings):
     config.scan('geoadmin.models')
     # Static config
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
 
+    config.add_route('home', '/')
     # Application specific
     config.add_route('hello', '/hello/{name}')
-    config.add_view(hello_world, route_name='hello')
-
     config.add_route('layers', '/layers/{id}')
-    config.add_view(layers, route_name='layers', renderer='json')
+    config.add_route('mapservice', '/rest/services/{map}/MapServer')
 
     config.add_subscriber(add_render_globals,BeforeRender)
-    config.scan()
+    config.scan() # required to find code decorated by view_config
     return config.make_wsgi_app()
--- geoadmin/models/bod.py
+++ geoadmin/models/bod.py
@@ -9,8 +9,36 @@ Base = bases['bod']
 
 
 
+class BodLayerDe(Base):
+    __tablename__ = 'view_bod_layer_info_de'
+    #__table_args__ = ({'autoload': True})
+    id = Column('bod_layer_id', Text, primary_key=True)
+    bezeichnung = Column('bezeichnung', Text)
+    abstract = Column('abstract', Text)
+
 class BodLayerFr(Base):
     __tablename__ = 'view_bod_layer_info_fr'
+    #__table_args__ = ({'autoload': True})
+    id = Column('bod_layer_id', Text, primary_key=True)
+    bezeichnung = Column('bezeichnung', Text)
+    abstract = Column('abstract', Text)
+
+class BodLayerIt(Base):
+    __tablename__ = 'view_bod_layer_info_it'
+    #__table_args__ = ({'autoload': True})
+    id = Column('bod_layer_id', Text, primary_key=True)
+    bezeichnung = Column('bezeichnung', Text)
+    abstract = Column('abstract', Text)
+
+class BodLayerRm(Base):
+    __tablename__ = 'view_bod_layer_info_rm'
+    #__table_args__ = ({'autoload': True})
+    id = Column('bod_layer_id', Text, primary_key=True)
+    bezeichnung = Column('bezeichnung', Text)
+    abstract = Column('abstract', Text)
+
+class BodLayerEn(Base):
+    __tablename__ = 'view_bod_layer_info_en'
     #__table_args__ = ({'autoload': True})
     id = Column('bod_layer_id', Text, primary_key=True)
     bezeichnung = Column('bezeichnung', Text)
