788e615d9d4d6a9303cc7382a663380a75e74fdd
gjn <gjn@space.ch>
2013-04-12 08:50:45 +0200

    Separating dialog and graphics component. (Graphics is used in multiple locations)

--- geoadmin/static/lib/geoadmin/src/ga/ui/profile-graphics.js
+++ geoadmin/static/lib/geoadmin/src/ga/ui/profile-graphics.js
@@ -1,39 +1,12 @@
 
 goog.provide('ga.ui.Profile');
-goog.provide('ga.ui.Profile.Dialog');
-//TODO: probably move into seperate file, because it could be used elsewhere too
 goog.provide('ga.ui.Profile.Graphics');
 
 goog.require('ga.model.Profile');
 
 goog.require('goog.ui.Control');
-goog.require('goog.ui.Dialog');
 goog.require('goog.graphics');
 goog.require('goog.array');
-
-/**
- * @constructor
- * @extends {goog.ui.Dialog}
- * @param opt_domHelper 
- */
-ga.ui.Profile.Dialog = function (opt_domHelper) {
-    'use strict';
-    goog.ui.Dialog.call(this, 'profile-dialog', undefined, opt_domHelper);
-
-    this.setTitle('This is your profile!');
-    this.setModal(false);
-
-    this.graphics_ = new ga.ui.Profile.Graphics();
-    this.addChild(this.graphics_, true);
-};
-
-goog.inherits(ga.ui.Profile.Dialog, goog.ui.Dialog);
-
-ga.ui.Profile.Dialog.prototype.update = function (model) {
-    'use strict';
-    this.graphics_.draw(model);
-};
-
 
 /*
  * Simple container to hold a goog.graphics.AbstractGraphics which is
@@ -73,7 +46,7 @@ ga.ui.Profile.Graphics.prototype.draw = function (model) {
     canv.setCoordOrigin(mod.boundingRect().left, mod.boundingRect().top);
     canv.setCoordSize(mod.boundingRect().width, mod.boundingRect().height);
 
-    var stroke = new goog.graphics.Stroke(50, 'red');
+    var stroke = new goog.graphics.Stroke(50, 'white');
     var path = new goog.graphics.Path();
     var init = false;
     goog.array.map(mod.outPoints(), function (point) {
