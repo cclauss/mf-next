52b6081704e8d159d5129eab0e0e2d163eaa05c2
loicgasser <loicgasser4@gmail.com>
2013-04-22 14:25:06 +0200

    add a few tests

--- geoadmin/tests/functional/test_mapservice.py
+++ geoadmin/tests/functional/test_mapservice.py
@@ -6,6 +6,15 @@ class TestMapServiceView(TestsBase):
 
     def test_index_no_parameters(self):
         resp = self.testapp.get('/rest/services/geoadmin/MapServer', status=200)
+        self.failUnless(resp.content_type == 'application/json')
+
+    def test_index_with_searchtext(self):
+        resp = self.testapp.get('/rest/services/geoadmin/MapServer', params={'searchText':'wasser'}, status=200)
+        self.failUnless(resp.content_type == 'application/json')
+
+    def test_index_with_cb(self):
+        resp = self.testapp.get('/rest/services/geoadmin/MapServer', params={'cb':'cb'}, status=200)
+        self.failUnless(resp.content_type == 'application/javascript')
 
     def test_index_with_cb(self):
         resp = self.testapp.get('/rest/services/geoadmin/MapServer', params={'cb':'cb'}, status=200)
