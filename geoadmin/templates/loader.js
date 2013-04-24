a5545c538e7c5ed9d4c3fde52f0fb4543bf2c548
loicgasser <loicgasser4@gmail.com>
2013-04-24 17:30:22 +0200

    add proj4js in loader.js

--- geoadmin/templates/loader.js
+++ geoadmin/templates/loader.js
@@ -5,6 +5,7 @@
     document.write('<link rel="stylesheet" type="text/css" href="' + "${h.versioned(request.static_url('geoadmin:static/build/ol3.css'))}" + '" />');
     document.write('<link rel="stylesheet" type="text/css" href="' + "${h.versioned(request.static_url('geoadmin:static/build/ga.css'))}" + '" />');
     // load the javascript
+    document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('geoadmin:static/lib/proj4js/proj4js-compressed.js'))}" + '"></scr' + 'ipt>');
     document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('geoadmin:static/build/geoadmin.js'))}" + '"></scr' + 'ipt>');
 })();
 </script>
