4404c83d4d5d006058b1c8e0f2dd85a41c42a90c
loicgasser <loicgasser4@gmail.com>
2013-04-30 09:58:49 +0200

    add the possibility to load the api in debug mode

--- geoadmin/templates/loader.js
+++ geoadmin/templates/loader.js
@@ -1,11 +1,20 @@
+<% mode = request.params.get('mode') %>
+
 <script type="text/javascript">
 (function() {
     // load the css
     document.write('<link rel="stylesheet" type="text/css" href="' + "${h.versioned(request.static_url('geoadmin:static/build/closure.css'))}" + '" />');
     document.write('<link rel="stylesheet" type="text/css" href="' + "${h.versioned(request.static_url('geoadmin:static/build/ol3.css'))}" + '" />');
     document.write('<link rel="stylesheet" type="text/css" href="' + "${h.versioned(request.static_url('geoadmin:static/build/ga.css'))}" + '" />');
-    // load the javascript
+// load the javascript
     document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('geoadmin:static/lib/proj4js/proj4js-compressed.js'))}" + '"></scr' + 'ipt>');
+    document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('geoadmin:static/lib/proj4js/defs/EPSG21781.js'))}" + '"></scr' + 'ipt>');
+% if mode == 'debug':
+    document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('geoadmin:static/lib/closure/closure/goog/base.js'))}" + '"></scr' + 'ipt>');
+    document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('geoadmin:static/build/ol3_deps.js'))}" + '"></scr' + 'ipt>');
+    document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('geoadmin:static/build/ga_deps.js'))}" + '"></scr' + 'ipt>');
+% else:
     document.write('<scr' + 'ipt type="text/javascript" src="' + "${h.versioned(request.static_url('geoadmin:static/build/geoadmin.js'))}" + '"></scr' + 'ipt>');
+% endif
 })();
 </script>
