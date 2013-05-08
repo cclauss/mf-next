34c2932508b9b208c7d9a8f84c38cbc6009b26e3
loicgasser <loicgasser4@gmail.com>
2013-05-08 14:33:14 +0200

    change parameter validation, add tests, correct one test, geometryFormat instead of f

--- geoadmin/lib/validation.py
+++ geoadmin/lib/validation.py
@@ -4,63 +4,100 @@ import pyramid.httpexceptions as exc
 from geoadmin.models import models_from_name
 from geoadmin.lib.helpers import check_even
 
-def validateGeometry(request):
-    geom = request.params.get('geometry')
-    if geom is None:
-        raise exc.HTTPBadRequest('Please provide the parameter geometry  (Required)')
-    geom = geom.split(',')
-    if check_even(len(geom)) is False:
-        raise exc.HTTPBadRequest('Please provide an even number of float numbers for the parameter geometry')
-    try:
-        geometry = map(float, geom)
-    except ValueError:
-        raise exc.HTTPBadRequest('Please provide numerical values in a comma separated list for the parameter geometry')
-    return geometry
 
-def validateGeometryType(request):
-    esriTypes = ['esriGeometryPoint', 'esriGeometryPolyline', 'esriGeometryPolygon', 'esriGeometryEnvelope']
-    geometryType = request.params.get('geometryType')
-    if geometryType is None:
-        raise exc.HTTPBadRequest('Please provide the parameter geometryType  (Required)')
-    if geometryType not in esriTypes:
-        raise exc.HTTPBadRequest('Please provide a valid geometry type')
-    return geometryType
+class Validation(object):
+    def __init__(self):
+        self._geometry = None
+        self._geometryType = None
+        self._imageDisplay = None
+        self._mapExtent = None
+        self._tolerance = None
+        self._models = None
+        self.esriGeometryTypes = ['esriGeometryPoint', 'esriGeometryPolyline', 'esriGeometryPolygon', 'esriGeometryEnvelope']
 
-def validateImageDisplay(request):
-    imgDisplay = request.params.get('imageDisplay')
-    if imgDisplay is None:
-        raise exc.HTTPBadRequest('Please provide the parameter imageDisplay  (Required)')
-    imgDisplay = imgDisplay.split(',')
-    if len(imgDisplay) != 3:
-        raise exc.HTTPBadRequest('Please provide the parameter imageDisplay in a comma separated list of 3 arguments')
-    try:
-        imageDisplay = map(float, imgDisplay)
-    except ValueError:
-        raise exc.HTTPBadRequest('Please provide numerical values for the parameter imageDisplay')
-    return imageDisplay
+    @property
+    def geometry(self):
+        return self._geometry
 
-def validateMapExtent(request):
-    mapExtent = request.params.get('mapExtent')
-    if mapExtent is None:
-        raise exc.HTTPBadRequest('Please provide the parameter mapExtent  (Required)')
-    mapExtent = mapExtent.split(',')
-    if len(mapExtent) != 4:
-        raise exc.HTTPBadRequest('The parameter mapExtent must contain 4 coordinates in a comma-separated list')
-    try:
-        mapExtent = map(float, mapExtent)
-    except ValueError:
-        raise exc.HTTPBadRequest('Please provide numerical values for the parameter mapExtent')
-    return mapExtent
+    @property
+    def geometryType(self):
+        return self._geometryType
 
-def validateTolerance(request):
-    tolerance = request.params.get('tolerance')
-    if tolerance is None:
-        raise exc.HTTPBadRequest('Please provide the parameter tolerance (Required)')
-    try:
-        tolerance = float(tolerance)
-    except ValueError:
-        raise exc.HTTPBadRequest('Please provide an integer value for the pixel tolerance')
-    return tolerance
+    @property
+    def imageDisplay(self):
+        return self._imageDisplay
+
+    @property
+    def mapExtent(self):
+        return self._mapExtent
+
+    @property
+    def tolerance(self):
+        return self._tolerance
+
+    @property
+    def models(self):
+        return self._models
+
+    @geometry.setter
+    def geometry(self, value):
+        if value is None:
+            raise exc.HTTPBadRequest('Please provide the parameter geometry  (Required)')
+        value = value.split(',')
+        if check_even(len(value)) is False:
+            raise exc.HTTPBadRequest('Please provide an even number of float numbers for the parameter geometry')
+        try:
+            self._geometry = map(float, value)
+        except ValueError:
+            raise exc.HTTPBadRequest('Please provide numerical values in a comma separated list for the parameter geometry')
+
+    @geometryType.setter
+    def geometryType(self, value):
+        if value is None:
+            raise exc.HTTPBadRequest('Please provide the parameter geometryType  (Required)')
+        if value not in self.esriGeometryTypes:
+            raise exc.HTTPBadRequest('Please provide a valid geometry type')
+        self._geometryType = value
+
+    @imageDisplay.setter
+    def imageDisplay(self, value):
+        if value is None:
+            raise exc.HTTPBadRequest('Please provide the parameter imageDisplay  (Required)')
+        value = value.split(',')
+        if len(value) != 3:
+            raise exc.HTTPBadRequest('Please provide the parameter imageDisplay in a comma separated list of 3 arguments')
+        try:
+            self._imageDisplay = map(float, value)
+        except ValueError:
+            raise exc.HTTPBadRequest('Please provide numerical values for the parameter imageDisplay')
+
+    @mapExtent.setter
+    def mapExtent(self, value):
+        if value is None:
+            raise exc.HTTPBadRequest('Please provide the parameter mapExtent  (Required)')
+        value = value.split(',')
+        if len(value) != 4:
+            raise exc.HTTPBadRequest('The parameter mapExtent must contain 4 coordinates in a comma-separated list')
+        try:
+            self._mapExtent = map(float, value)
+        except ValueError:
+            raise exc.HTTPBadRequest('Please provide numerical values for the parameter mapExtent')
+
+    @tolerance.setter
+    def tolerance(self, value):
+        if value is None:
+            raise exc.HTTPBadRequest('Please provide the parameter tolerance (Required)')
+        try:
+            self._tolerance = float(value)
+        except ValueError:
+            raise exc.HTTPBadRequest('Please provide an integer value for the pixel tolerance')
+
+    @models.setter
+    def models(self, value):
+        if value is None:
+            raise exc.HTTPBadRequest('Please provide a valid layer Id')
+        self._models = value
+
 
 def validateLayerId(idlayer):
     models = models_from_name(idlayer)
--- geoadmin/views/mapservice.py
+++ geoadmin/views/mapservice.py
@@ -5,18 +5,16 @@ from pyramid.view import view_config
 from geoadmin.models import Session, models_from_name
 from geoadmin.models.bod import get_bod_model, computeHeader
 from geoadmin.lib.helpers import locale_negotiator
-from geoadmin.lib.validation import (
-    validateGeometry, validateGeometryType, 
-    validateImageDisplay, validateMapExtent,
-    validateTolerance, validateLayerId)
+from geoadmin.lib.validation import Validation, validateLayerId
 
 import logging
 logging.basicConfig()
 logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
-class MapService(object):
+class MapService(Validation):
 
     def __init__(self, request):
+        super(MapService, self).__init__()
         self.request = request
         self.mapName = request.matchdict.get('map') # The topic
         self.cbName = request.params.get('cb')
@@ -25,14 +23,13 @@ class MapService(object):
         self.translate = request.translate
 
     @view_config(route_name='mapservice', renderer='jsonp')    
-    def index(self):
+    def metadata(self):
         model = get_bod_model(self.lang)
         results = computeHeader(self.mapName)
-        session = Session()
-        query = session.query(model).filter(model.maps.ilike('%%%s%%' % self.mapName))
+        query = Session.query(model).filter(model.maps.ilike('%%%s%%' % self.mapName))
         query = self.fullTextSearch(query, model.fullTextSearch)
         layers = [layer.layerMetadata() for layer in query]
-        session.close()
+        query.session.close()
         results['layers'].append(layers)
         return results
 
@@ -41,12 +38,11 @@ class MapService(object):
         from pyramid.renderers import render_to_response
         idlayer = self.request.matchdict.get('idlayer')
         model = get_bod_model(self.lang)
-        session = Session()
-        query = session.query(model).filter(model.maps.ilike('%%%s%%' % self.mapName))
+        query = Session.query(model).filter(model.maps.ilike('%%%s%%' % self.mapName))
         query = query.filter(model.idBod==idlayer)
         for layer in query:
             legend = {'layer': layer.layerMetadata()}
-        session.close()
+        query.session.close()
         response = render_to_response('geoadmin:templates/legend.mako',
                                         legend,
                                         request = self.request)
@@ -54,34 +50,30 @@ class MapService(object):
             return response 
         else:
             return response.body
-            
 
     # order matters, last route is the default!
-    @view_config(route_name='identify', renderer='geojson', request_param='f=geojson')
+    @view_config(route_name='identify', renderer='geojson', request_param='geometryFormat=geojson')
     def view_identify_geosjon(self):
         return self.identify()
-
 
     @view_config(route_name='identify', renderer='esrijson')
     def view_identify_esrijson(self):
         return self.identify()
 
-
     def identify(self):
-        self.geometry = validateGeometry(self.request)
-        self.geometryType = validateGeometryType(self.request)
-        self.imageDisplay = validateImageDisplay(self.request)
-        self.mapExtent = validateMapExtent(self.request)
-        self.tolerance = validateTolerance(self.request)
+        self.geometry = self.request.params.get('geometry')
+        self.geometryType = self.request.params.get('geometryType')
+        self.imageDisplay = self.request.params.get('imageDisplay')
+        self.mapExtent = self.request.params.get('mapExtent')
+        self.tolerance = self.request.params.get('tolerance')
         self.returnGeometry = self.request.params.get('returnGeometry', True)
-        features = list()
         layers = self.request.params.get('layers','all')
         models = self.getModelsFromLayerName(layers)
         queries = list(self.buildQueries(models))
         features = list(self.getFeaturesFromQueries(queries))
         return {'results': features} 
 
-    @view_config(route_name='getfeature', renderer='geojson', request_param='f=geojson')
+    @view_config(route_name='getfeature', renderer='geojson', request_param='geometryFormat=geojson')
     def view_getfeature_geojson(self):
         return self.getfeature()
 
@@ -111,13 +103,12 @@ class MapService(object):
         idfeature = self.request.matchdict.get('idfeature')
         idlayer = self.request.matchdict.get('idlayer') 
         model = validateLayerId(idlayer)[0]
-        session = Session()
-        query = session.query(model).filter(model.id==idfeature)
+        query = Session.query(model).filter(model.id==idfeature)
         if self.returnGeometry:
             feature = [f.__geo_interface__ for f in query]
         else:
             feature = [f.interface for f in query]
-        session.close()
+        query.session.close()
         feature = {'feature': feature.pop()} if len(feature) > 0 else {'feature': []}
         template = model.__template__
         return feature, template
@@ -137,7 +128,6 @@ class MapService(object):
     def buildQueries(self, models):
         for layer in models:
             for model in layer:
-                session = Session()
                 geom_filter = model.geom_filter(
                     self.geometry,
                     self.geometryType,
@@ -145,14 +135,11 @@ class MapService(object):
                     self.mapExtent,
                     self.tolerance
                 )
-                query = session.query(model).filter(geom_filter)
+                query = Session.query(model).filter(geom_filter)
                 query = self.fullTextSearch(query, model.display_field())
                 yield query
 
     def getModelsFromLayerName(self, layers_param):
-        ''' As a layer can possess multiple models,
-        this function returns lists of models within 
-        a main list'''
         layers = self.getLayerListFromMap() if layers_param == 'all' else layers_param.split(':')[1].split(',')
         models = [
             validateLayerId(layer) for
@@ -163,7 +150,6 @@ class MapService(object):
 
     def getLayerListFromMap(self):
         model = get_bod_model(self.lang)
-        session = Session()
         query = Session.query(model).filter(
             model.maps.ilike('%%%s%%' % self.mapName)
         )
@@ -173,4 +159,5 @@ class MapService(object):
             q in query
             if models_from_name(q.idBod) is not None
         ]
+        query.session.close()
         return layerList
